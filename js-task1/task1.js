449f2c2f1ac2b2a4b7662c02becebce1361629bb
00x77 <767961263@qq.com>
2021-03-11 10:26:50 +0800

    js1

--- js-task1/task1.js
+++ js-task1/task1.js
@@ -7,26 +7,26 @@ function check(one, two, three) {
 
 function colors() {
     var rgb = new Array();
-    for (var i = 0; i < 3;) {
+    for (var i = 0; i < 3;) {                              //生成一个随机颜色，随机三次
         var r = Math.floor(Math.random() * 256);
         var g = Math.floor(Math.random() * 256);
         var b = Math.floor(Math.random() * 256);
-        if (!(r == 255 && g == 165 && b == 0)) {
+        if (!(r == 255 && g == 165 && b == 0)) {           //如果生成颜色不是橙色，则给数组添加值，并自增
             rgb[i] = "(" + r + "," + g + "," + b + ")";
             i++;
         }
     }
-    if (check(rgb[0], rgb[1], rgb[2])) {
-        return rgb;
+    if (check(rgb[0], rgb[1], rgb[2])) {                   //如果没有相同的颜色，结果为true，
+        return rgb;                                        //就返回值
     } 
-    else {
+    else {                                                 //如果有相同的颜色，就重复函数
         return colors();
     }
 }
 
 function begin(one, two, three) {
     if (one == two || two == three || one == three) {
-        if (one == two) {
+        if (one == two) {                                   //如果有重复的格子，则给一个新的值
             one = Math.floor(Math.random() * list.length);
         }
         else if (two == three) {
@@ -35,32 +35,32 @@ function begin(one, two, three) {
         else if (one == three) {
             three = Math.floor(Math.random() * list.length);
         }
-        begin(one, two, three);
+        begin(one, two, three);                             //重复过程，直到没有重复值
     }
     else {
         var color = colors();
-        list[one].style.backgroundColor = "rgb" + color[0];
+        list[one].style.backgroundColor = "rgb" + color[0];  //给三个格子赋颜色
         list[two].style.backgroundColor = "rgb" + color[1];
         list[three].style.backgroundColor = "rgb" + color[2];
     }
 }
 
 function run() {
-    clearInterval(time);
-    time = setInterval(function () {
-        for (var i = 0; i < list.length; i++) {
-            list[i].style.backgroundColor = "orange";
+    clearInterval(time);                                 //停止计时事件
+    time = setInterval(function () {                     
+        for (var i = 0; i < list.length; i++) {          //给九宫格重置颜色
+            list[i].style.backgroundColor = "#ffa500";
         }
-        var one = Math.floor(Math.random() * list.length);
+        var one = Math.floor(Math.random() * list.length);//随机选择三个格子
         var two = Math.floor(Math.random() * list.length);
         var three = Math.floor(Math.random() * list.length);
-        begin(one, two, three);
+        begin(one, two, three);                          //去重
     }, 1000);
 }
 
 function stop() {
-    clearInterval(time);
-    for (var i = 0; i < list.length; i++) {
-        list[i].style.backgroundColor = "orange";
+    clearInterval(time);                                  //停止计时事件
+    for (var i = 0; i < list.length; i++) {               //给九宫格重置颜色
+        list[i].style.backgroundColor = "#ffa500";
     }
 }
