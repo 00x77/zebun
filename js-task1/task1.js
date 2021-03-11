069cc40bdd7df5aef3093622811b992b84d0d520
00x77 <767961263@qq.com>
2021-03-11 09:47:50 +0800

    js-1

--- js-task1/task1.js
+++ js-task1/task1.js
@@ -1,38 +1,53 @@
 var list = document.getElementsByClassName("box");
 var time;
 
+function check(one, two, three) {
+    return one != two && two != three && one != three;
+}
+
+function colors() {
+    var rgb = new Array();
+    for (var i = 0; i < 3;) {
+        var r = Math.floor(Math.random() * 256);
+        var g = Math.floor(Math.random() * 256);
+        var b = Math.floor(Math.random() * 256);
+        if (!(r == 255 && g == 165 && b == 0)) {
+            rgb[i] = "(" + r + "," + g + "," + b + ")";
+            i++;
+        }
+    }
+    if (check(rgb[0], rgb[1], rgb[2])) {
+        return rgb;
+    } 
+    else {
+        return colors();
+    }
+}
+
 function begin(one, two, three) {
     if (one == two || two == three || one == three) {
         if (one == two) {
             one = Math.floor(Math.random() * list.length);
-        } 
+        }
         else if (two == three) {
             two = Math.floor(Math.random() * list.length);
-        } 
+        }
         else if (one == three) {
             three = Math.floor(Math.random() * list.length);
         }
         begin(one, two, three);
-    } 
-    else {
-        list[one].style.backgroundColor = "rgb" + colors();
-        list[two].style.backgroundColor = "rgb" + colors();
-        list[three].style.backgroundColor = "rgb" + colors();
     }
-}  //随机选择三个方块
-
-function colors() {
-    var rgb;
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-    rgb = "(" + r + "," + g + "," + b + ")";
-    return rgb;
-}  //生成一个随机颜色
+    else {
+        var color = colors();
+        list[one].style.backgroundColor = "rgb" + color[0];
+        list[two].style.backgroundColor = "rgb" + color[1];
+        list[three].style.backgroundColor = "rgb" + color[2];
+    }
+}
 
 function run() {
     clearInterval(time);
-    time = setInterval(function() {
+    time = setInterval(function () {
         for (var i = 0; i < list.length; i++) {
             list[i].style.backgroundColor = "orange";
         }
@@ -40,12 +55,12 @@ function run() {
         var two = Math.floor(Math.random() * list.length);
         var three = Math.floor(Math.random() * list.length);
         begin(one, two, three);
-    },1000);
-}  //按钮点击执行颜色更改
+    }, 1000);
+}
 
 function stop() {
     clearInterval(time);
     for (var i = 0; i < list.length; i++) {
         list[i].style.backgroundColor = "orange";
     }
-}  //按钮点击停止颜色更改
+}
